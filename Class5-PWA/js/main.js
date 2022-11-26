a845c4577a16700f0d502f430a3e57442070fc8a
nastiazi <82148426+nastiazi@users.noreply.github.com>
2022-11-26 15:18:10 +0200

    Add files via upload

---
+++ Class5-PWA/js/main.js
@@ -0,0 +1,44 @@
+﻿var projects = [
+    { name: "Audio Tunnel", image: "images/img_1.jpg" },
+    { name: "MonsteRow", image: "images/img_2.jpg" },
+    { name: "פיזיוקיד", image: "images/img_3.jpg" },
+    { name: "קפוץ לקרחון", image: "images/img_4.jpg" },
+    { name: " פרטיול", image: "images/img_5.jpg" },
+    { name: "מכונת הזמן", image: "images/img_6.jpg" },
+    { name: "מכבי באדי", image: "images/img_7.png" },
+    { name: "לימפי", image: "images/img_8.png" },
+    { name: "לומדה להכשרת עובדי כלביה", image: "images/img_9.png" },
+]
+
+function createProjectsList() {
+    var output = "";
+    for (i = 0; i < projects.length; i++) {
+        output += `
+        <div class="col">
+            <li class="card">
+                <img class="bd-placeholder-img card-img-top" src=${projects[i].image} />
+                <div class="card-body">
+                    <h3 class="card-text">${projects[i].name}</h1>
+                    <a class="btn btn-primary d-block" href="#">צפייה</a>
+                </div>
+            </li>
+        </div>
+        `
+    }
+    document.getElementById("projects-list").innerHTML = output
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    createProjectsList();
+});
+
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("../service-worker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
---
+++ Class5-PWA/service-worker.js
@@ -0,0 +1,50 @@
+﻿var cacheName = "telem-projects-cache-v1"
+//מערך עם כל הדברים הסטטים שיש לנו 
+//אנחנו שמים את כל הנתיבים שיש לנו בתוך הפרויקט
+var assets = [
+    "./",
+    "index.html",
+    "css/style.css",
+    "css/bootstrap.rtl.min.css",
+    "js/main.js",
+    "js/bootstrap.bundle.js",
+    "images/img_1.jpg",
+    "images/img_2.jpg",
+    "images/img_3.jpg",
+    "images/img_4.jpg",
+    "images/img_5.jpg",
+    "images/img_6.jpg",
+    "images/img_7.png",
+    "images/img_8.png",
+    "images/img_9.png",
+]
+
+
+//בהתקנה הראשונה של האתר, נפתח זכרון מטמון ונוסיף אליו את הקבצים
+//לך לזכרון המטמון
+//ותפתח קאצ' חדש (זכרון מטמון חדש) עם השם שהגדרתי
+//ואחריי זה
+//תוסיף לקאש שפתחת את כל המשתנים שפתחתי
+//המשתנים assests שנפתח מקודם
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+//fetch - פנייה לשרת, קריאה לשרת ולהביא ממנו מידע
+//הקבצים שאיתם אנחנו עובדים איתם
+//אל תביא ישר אותם, לך לcaches שלי
+//ולבדוק האם יש משהו שתואם את הבקשה הזאת
+//במידה וכן, response 
+//במידה ולא אז תעשה fetch עם הבקשה ששלחתי
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
